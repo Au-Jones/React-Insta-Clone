db341d16228cbd1640226d8ca8f003781e52d748
Your Name <aujones95@gmail.com>
2019-05-06 16:14:17 -0400

    finished searchbar with references

--- instagram/src/components/CommentSection.js
+++ instagram/src/components/CommentSection.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import Comment from './Comment';
-import CommentInput from './CommentInput';
+import NewComment from './EnterNewComment';
 
 
 class Comments extends React.Component {
--- instagram/src/components/SearchBar.js
+++ instagram/src/components/SearchBar.js
@@ -1,11 +1,28 @@
 import React from 'react'
-
-const search = props => {
+import IGLogo from '../../assests/iglogo.png';
+import './searchbar.css';
+const search = () => {
     return (
-        <div>
-            <h1>Search</h1>
+        <div className="search-bar">
+        <div className="image">
+          <img alt="instagram logo" src={IGLogo} className="logo-image" />
         </div>
+        <div>
+          <input type="text" placeholder="Search" />
+        </div>
+        <div className="social-Container">
+          <div className="social">
+            <i className="fa fa-compass" />
+          </div>
+          <div className="social">
+            <i className="fa fa-heart" />
+          </div>
+          <div className="social">
+            <i className="fa fa-user-circle" />
+          </div>
+        </div>
+      </div>
     );
-};
-
-export default search;
+  };
+  
+  export default search;
