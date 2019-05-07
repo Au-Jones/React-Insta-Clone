43c79b9187f128b78c3671eee47d531988f63a7e
Your Name <aujones95@gmail.com>
2019-05-07 18:55:03 -0400

    saving

--- instagram-app/src/components/CommentSection.js
+++ instagram-app/src/components/CommentSection.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-import CommentSection from './Comment';
+import Comment from './Comment';
 import NewComment from './components/NewComment';
 
 
@@ -15,35 +15,35 @@ class CommentSection extends React.Component {
 
     componentDidMount() {
         const id = this.props.postId
-        this.setComments();
+
 
 
     }
+
+
+    commentHandler = e => {
+        this.setState({ comment: e.target.value });
+    };
+
+
+
+    render() {
+        return (
+            <div>
+                {this.state.comments.map((c, i) => <Comment key={i} comment={c} />)}
+                <NewComment
+                    comment={this.state.comment}
+                    changeComment={this.commentHandler}
+                />
+            </div>
+        );
+    }
+
+
+    CommentSection.propTypes = {
+        comment: PropTypes.arrayOf(
+            PropTypes.shape({ text: PropTypes.string, username: PropTypes.string })
+        )
+    };
 }
-
-commentHandler = e => {
-    this.setState({ comment: e.target.value });
-};
-
-
-
-render() {
-    return (
-        <div>
-            {this.state.comments.map((c, i) => <Comment key={i} comment={c} />)}
-            <NewComment
-            comment ={this.state.comment}
-            changeComment={this.commentHandler}
-            />
-        </div>
-    );
-}
-}
-
-comments.propTypes = {
-    comment: PropTypes.arrayOf(
-        PropTypes.shape({ text: PropTypes.string, username: PropTypes.string })
-    )
-};
-
 export default CommentSection;
